ac408859d10b4543c8020dd27b55dc43a6e8c72e
Dmitry <dim4ik7558118@gmail.com>
2021-03-01 23:16:54 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 module.exports = function towelSort (matrix) {
 	if(matrix == undefined) return [];
 	let newArr = [];
-	for (var i = 0; i < matrix.length; i++) {
-	if(i % 2 == 1) matrix[i].sort(( a , b )=> b - a);
-	for (var j= 0; j < matrix[i].length; j++) {
-		newArr.push(matrix[i][j]);
+	for (let i = 0; i < matrix.length; i++) {
+		if(i % 2 == 1) matrix[i].reverse();
+		for (let j= 0; j < matrix[i].length; j++) {
+			newArr.push(matrix[i][j]);
 		}
 	}
         return newArr;
